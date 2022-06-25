511dc3dfcc2d0d91c890fa0a7356b7fbc819d7db
Sergi <sergisc78@gmail.com>
2022-06-25 19:01:01 +0200

    Modificacions finals

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+
+    apiKey: "AIzaSyDIGrW1cjTuThHs-4VlUgXGAnfKOmvrmAk",
+
+    authDomain: "logincostumers.firebaseapp.com",
+
+    projectId: "logincostumers",
+
+    storageBucket: "logincostumers.appspot.com",
+
+    messagingSenderId: "1072359041837",
+
+    appId: "1:1072359041837:web:8a9205a242894fc0e101a4",
+
+    measurementId: "G-RKZ7XJPCZM"
+  }
 };
